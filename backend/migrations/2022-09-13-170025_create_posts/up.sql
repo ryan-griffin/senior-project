a9175384d0687d1077d2d30ebf0c3ee369037bce
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-09-13 14:06:21 -0400

    started database

---
+++ backend/migrations/2022-09-13-170025_create_posts/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  body TEXT NOT NULL,
+  date TEXT NOT NULL
+)
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,25 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use diesel::prelude::*;
+use senior_project::models::*;
+use senior_project::*;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use self::schema::posts::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = posts
+        .limit(5)
+        .load::<Post>(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
+
     HttpServer::new(|| {
         App::new().route(
             "/",
