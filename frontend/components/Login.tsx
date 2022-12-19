1e5bd424ee12b64274811591f958fac6eb74c7ea
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-12-19 09:44:50 -0500

    fix

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -13,7 +13,6 @@ const Login: FC<Props> = ({ visible, setVisible }) => {
     const [mode, setMode] = useState<"login" | "sign-up">("login");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const containerClass = visible == true ? "flex" : "hidden";
 
     async function auth(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -35,7 +34,9 @@ const Login: FC<Props> = ({ visible, setVisible }) => {
 
     return (
         <div
-            className={`fixed top-0 left-0 z-20 h-screen w-screen bg-black/50 p-4 justify-center items-center ${containerClass}`}
+            className={`fixed top-0 left-0 z-20 h-screen w-screen bg-black/50 p-4 justify-center items-center ${
+                visible ? "flex" : "hidden"
+            }`}
         >
             <form
                 className="flex flex-col p-4 gap-4 bg-white rounded-lg shadow-md w-[344px] animate-grow"
